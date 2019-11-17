06690505171da23fe59b65fe6b8170a45449f88f
ChoeJiwon <jiwon0929@ajou.ac.kr>
2019-11-17 17:42:29 +0900

    delete spaceTab

--- src/routes/users.js
+++ src/routes/users.js
@@ -11,19 +11,19 @@ router.use(bodyParser.json());
 /* POST Check whether gollect user or not */
 router.post('/', (req, res, next) => {
 	const hash = req.body.user_hash;
-	
+
 	var sql = 'SELECT * FROM users WHERE hash = \'' + hash + '\'';
-	
+
 	var result = false;
-	
+
 	connection.query(sql, (err, rows, field) => {
 		if(err){
 			console.log('Error while performing gollect user check', err);
 		}
 		else{
 			if(rows.length > 0) result = true;
-			
-			res.json({
+
+			res.send({
 				result : result
 			});
 		}
@@ -33,18 +33,18 @@ router.post('/', (req, res, next) => {
 /* POST users Login */
 router.post('/login', (req, res, next) => {
 	const hash = req.body.user_hash;
-	
+
 	var sql = 'SELECT * FROM users WHERE hash =\'' + hash + '\'';
-	
+
 	var result = false;
-	
+
 	conection.query(sql, (err, rows, field) => {
 		if(err){
 			console.log('Error while performing Login by user_hash', err);
 		}
 		else{
 			if(rows.length > 0) result = true;
-			
+
 			res.json({
 				result: result,
 				users: rows
@@ -58,20 +58,20 @@ router.post('/signup', (req, res, next) => {
 	const hash = req.body.user_hash;
 	const email = req.body.user_email;
 	const name = req.body.user_name;
-	
+
 	var params = [hash, email, name];
-	
+
 	var sql = 'INSERT INTO users (hash, email, name) VALUES(?, ?, ?)';
-	
+
 	var result = false;
-	
+
 	connection.query(sql, params, (err, rows, fields) => {
 		if(err){
 			console.log('Error while performing user Sign up', err);
 		}
 		else{
 			result = true
-			
+
 			res.json({
 				result: result
 			});
